825d02aa94fbeced607c9dac0b7bfc5e62bc25c6
Vinícius Canedo <96919368+ViniciusCanedo@users.noreply.github.com>
2022-09-20 21:05:08 -0300

    passwordShow adicionado

--- js/page-scroll.js
+++ js/page-scroll.js
@@ -1,13 +1,13 @@
 const menuItems = document.querySelectorAll('#navbar a[href^="#"]');
 
-menuItems.forEach(item => {
-  item.addEventListener('click', scrollToId);
-})
+menuItems.forEach((item) => {
+  item.addEventListener("click", scrollToId);
+});
 
-function scrollToId(event){
+function scrollToId(event) {
   event.preventDefault();
   const element = event.target;
-  const id = element.getAttribute('href');
+  const id = element.getAttribute("href");
   const to = document.querySelector(id).offsetTop;
 
   window.scroll({
